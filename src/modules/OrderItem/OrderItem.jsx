82292947a616d3fbe48497bb112af09d9ffcc1cd
JulyDiv <july_developer@list.ru>
2023-04-28 14:42:39 +0700

    внесла правки в orderitem

--- src/modules/OrderItem/OrderItem.jsx
+++ src/modules/OrderItem/OrderItem.jsx
@@ -5,8 +5,6 @@ import axios from "axios";
 
 export const OrderItem = ({ getData, item }) => {
 
-  const [isOpenItem, setIsOpenItem] = useState(false);
-
   const onDelete = (data) => {
     console.log(data);
     axios
@@ -20,36 +18,11 @@ export const OrderItem = ({ getData, item }) => {
       });
   };
 
-  const onChangeStatus = (data) => {
-    console.log(data);
-    axios
-      .put(`${process.env.NEXT_PUBLIC_API_HOST}/order/${item.id}`, {
-        name: item.name,
-        phone: item.phone,
-        email: item.email,
-        merch: item.merch,
-        total: item.total,
-        status: "true",
-        sum: item.sum,
-      })
-      .then(({ data }) => {
-        console.log(data);
-        getData();
-      })
-      .catch(function (error) {
-        console.log(error.message);
-      });
-  };
-
   return (
     <>
       <div
         key={item ? item.id : ""}
-        className={
-          item && item.status === "true"
-            ? `${styles.block} ${styles.block_active}`
-            : `${styles.block}`
-        }
+        className={`${styles.block}`}
       >
         {item ? (
           <span className={`${styles.item} ${styles.id}`}>{item.id}</span>
@@ -73,37 +46,16 @@ export const OrderItem = ({ getData, item }) => {
           </span>
         )}
         {item ? (
-          <span
-            className={`${styles.item} ${styles.phone} ${
-              isOpenItem ? "" : `${styles.hidden}`
-            }`}
-          >
-            {item.phone}
-          </span>
+          <span className={`${styles.item} ${styles.phone}`}>{item.phone}</span>
         ) : (
-          <span
-            className={`${styles.item} ${styles.phone} ${styles.title} ${styles.hidden}`}
-          >
+          <span className={`${styles.item} ${styles.phone} ${styles.title}`}>
             Телефон
           </span>
         )}
-        {/* {item ? (
-          <span className={`${styles.item} ${styles.email}`} title={item.email}>
-            {item.email}
-          </span>
-        ) : (
-          <span className={`${styles.item} ${styles.email} ${styles.title}`}>
-            Почта
-          </span>
-        )} */}
         {item ? (
-          <span className={`${styles.item} ${styles.city} ${styles.hidden}`}>
-            {item.city}
-          </span>
+          <span className={`${styles.item} ${styles.city}`}>{item.city}</span>
         ) : (
-          <span
-            className={`${styles.item} ${styles.city} ${styles.title} ${styles.hidden}`}
-          >
+          <span className={`${styles.item} ${styles.city} ${styles.title}`}>
             Город
           </span>
         )}
@@ -114,60 +66,34 @@ export const OrderItem = ({ getData, item }) => {
             Сумма
           </span>
         )}
-
-        {/* {item ? (
-          <button
-            className={`${styles.item} ${styles.button} ${styles.hidden}`}
-            onClick={() => onChangeStatus(item.id)}
-          >
-            {item.status === "true" ? "Добавлен" : "Добавить"}
-          </button>
-        ) : (
-          <span
-            className={`${styles.item} ${styles.button} ${styles.title} ${styles.hidden}`}
-          >
-            Статус заказа
-          </span>
-        )} */}
-
         {item ? (
           <button
-            className={`${styles.item} ${styles.button} ${styles.hidden}`}
+            className={`${styles.item} ${styles.button}`}
             onClick={() => onDelete()}
           >
             Удалить
           </button>
         ) : (
-          <span
-            className={`${styles.item} ${styles.button} ${styles.title} ${styles.hidden}`}
-          >
+          <span className={`${styles.item} ${styles.button} ${styles.title}`}>
             Удалить
           </span>
         )}
         {item ? (
           <button
-            className={`${styles.item} ${styles.button} ${styles.hidden}`}
+            className={`${styles.item} ${styles.button}`}
             onClick={() => onDelete()}
           >
             Изменить
           </button>
         ) : (
-          <span
-            className={`${styles.item} ${styles.button} ${styles.title} ${styles.hidden}`}
-          >
+          <span className={`${styles.item} ${styles.button} ${styles.title}`}>
             Изменить
           </span>
         )}
         {item ? (
-          <button
-            className={`${styles.item} ${styles.button} ${styles.hidden}`}
-          >
-            Открыть
-          </button>
+          <button className={`${styles.item} ${styles.button}`}>Открыть</button>
         ) : (
-          <span
-            className={`${styles.item} ${styles.button} ${styles.title} ${styles.hidden}`}
-          >
+          <span className={`${styles.item} ${styles.button} ${styles.title}`}>
             Печать
           </span>
         )}
